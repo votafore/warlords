fb05a24dd5f9b0159ea3d3a039d4907dedd521a5
votafore <a.zakharov.druzh@gmail.com>
2016-09-20 09:20:09 +0300

    не большие доработки

--- app/src/main/java/com/votafore/warlords/net/CMWifi.java
+++ app/src/main/java/com/votafore/warlords/net/CMWifi.java
@@ -1,6 +1,11 @@
 package com.votafore.warlords.net;
 
+import android.content.Context;
+import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 /**
  * @author Votafore
@@ -9,22 +14,85 @@ import android.net.nsd.NsdServiceInfo;
  * provide connectivity via wi-fi
  */
 
-public class CMWifi implements IConnectivity{
+public class CMWifi implements IConnectivity, NsdManager.RegistrationListener{
 
-    public CMWifi(){
+    private String mServiceName = "WarlordsService";
 
-        NsdServiceInfo serviceInfo;
+    private Context mContext;
 
-        serviceInfo = new NsdServiceInfo();
-        serviceInfo.setServiceName("WarlordsService");
-        serviceInfo.setServiceType("_http._tcp.");
+    private boolean mValid = false;
 
+    public CMWifi(Context context) {
 
+        mContext = context;
+
+        ///////////////////////////////
+        // variant 2
+        ServerSocket socket;
+
+        try {
+            socket = new ServerSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        int port = socket.getLocalPort();
     }
 
 
     @Override
     public boolean init() {
-        return false;
+
+        final boolean result = false;
+
+        ///////////////////////////////
+        // https://developer.android.com/training/connect-devices-wirelessly/nsd.html
+        // variant 1
+        NsdServiceInfo serviceInfo;
+
+        serviceInfo = new NsdServiceInfo();
+        serviceInfo.setServiceName(mServiceName);
+        serviceInfo.setServiceType("_http._tcp.");
+        serviceInfo.setPort(1225);
+
+        NsdManager manager = (NsdManager) mContext.getSystemService(Context.NSD_SERVICE);
+
+        manager.registerService(serviceInfo, NsdManager.PROTOCOL_DNS_SD, this);
+
+        return result;
+    }
+
+    @Override
+    public void release(){
+
+        NsdManager manager = (NsdManager) mContext.getSystemService(Context.NSD_SERVICE);
+        manager.unregisterService(this);
+    }
+
+
+
+
+    @Override
+    public void onServiceRegistered(NsdServiceInfo serviceInfo) {
+
+        mServiceName = serviceInfo.getServiceName();
+
+        mValid = true;
+    }
+
+    @Override
+    public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+        mValid = false;
+    }
+
+    @Override
+    public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+
+    }
+
+    @Override
+    public void onServiceUnregistered(NsdServiceInfo serviceInfo) {
+
     }
 }
--- app/src/main/java/com/votafore/warlords/net/IConnectivity.java
+++ app/src/main/java/com/votafore/warlords/net/IConnectivity.java
@@ -9,4 +9,6 @@ package com.votafore.warlords.net;
 public interface IConnectivity {
 
     boolean init();
+
+    void release();
 }
